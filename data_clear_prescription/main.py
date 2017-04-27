d32c07da3d586676fbf5e33758a4a494778f008c
lzq <18248392627@163.com>
2017-04-27 17:50:00 +0800

    123

--- data_clear_prescription/main.py
+++ data_clear_prescription/main.py
@@ -68,7 +68,7 @@ class handlePrescription(object):
         self.infoDict["medicine"] = medicine(text, basicList, self.drugPath)
 
 
-    def handle(self,filename=None):
+    def handle(self,filenames=None):
         self.infoDict = {}
         result = {
             "prescription_information":[
@@ -90,6 +90,9 @@ class handlePrescription(object):
                 "费用":""
             }
         }
+        import zipfile
+        filename = zipfile.ZipFile(filenames)
+
         if not filename:
             if self.fileIndex:
                 currTime = time.strftime(r"%Y-%m-%d %H.%M.%S", time.localtime())
